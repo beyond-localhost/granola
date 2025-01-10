5a778da0ffa8b7cd20faca9fc24d5a7e62203ab1
steven semyung oh <semyung.dev@gmail.com>
2025-01-10 11:47:29 +0900

    implement draft calendar ui

--- frontend/src/routes/-components/calendar/calendar.tsx
+++ frontend/src/routes/-components/calendar/calendar.tsx
@@ -2,7 +2,10 @@ import * as React from "react";
 import {
   useCalendarGridContext,
   useCurrentDateContext,
+  type CalendarCell,
 } from "#/lib/todo-calendar";
+
+import { cn } from "#/lib/utils";
 
 function CalendarHeader() {
   const { next, prev, currentDate } = useCurrentDateContext();
@@ -10,27 +13,33 @@ function CalendarHeader() {
   const month = currentDate.getMonth() + 1;
 
   return (
-    <div className="w-full top-0 h-[80px] border-b pb-2 border-b-zinc-600">
-      <h1>
-        {year}년 {month}월
-      </h1>
-      <CalendarDays />
-      <button onClick={() => prev(currentDate, 3)}>이전달</button>
-      <button onClick={() => next(currentDate, 3)}>다음달</button>
+    <div className="w-full h-[80px] border-b border-b-zinc-300">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold">
+          {year}년 {month}월
+        </h1>
+        <div>
+          <button onClick={() => prev(currentDate, 3)}>이전달</button>
+          <button onClick={() => next(currentDate, 3)}>다음달</button>
+        </div>
+      </div>
+      <div className="mt-2">
+        <CalendarDays />
+      </div>
     </div>
   );
 }
 
 function CalendarDays() {
   return (
-    <div className="grid grid-cols-7 justify-items-end">
-      <div className="pr-2">일</div>
-      <div className="pr-2">월</div>
-      <div className="pr-2">화</div>
-      <div className="pr-2">수</div>
-      <div className="pr-2">목</div>
-      <div className="pr-2">금</div>
-      <div className="pr-2">토</div>
+    <div className="grid grid-cols-7 justify-items-end *:bg-sky-500">
+      <div>일</div>
+      <div>월</div>
+      <div>화</div>
+      <div>수</div>
+      <div>목</div>
+      <div>금</div>
+      <div>토</div>
     </div>
   );
 }
@@ -38,25 +47,39 @@ function CalendarDays() {
 function CalendarBody() {
   const grid = useCalendarGridContext();
   return (
-    <div className="grid grid-cols-7 grid-rows-6 h-[calc(100%-80px)]">
-      {grid.map((week, index) => {
+    <div className="grid grid-cols-7 grid-rows-6 justify-items-end h-[calc(100%-80px)]">
+      {grid.map((week, rowIndex, { length: rowLength }) => {
         return (
-          <React.Fragment key={index}>
-            {week.map((date) => {
+          <React.Fragment key={rowIndex}>
+            {week.map((date, columnIndex, { length: columnLength }) => {
               return (
-                <div key={date.key}>
-                  <span>{date.meta.date}</span>
-                  <ul>
-                    {date.todos.map((todo) => {
-                      return <li key={todo.id}>{todo.flakeName}</li>;
-                    })}
-                  </ul>
+                <div
+                  key={date.key}
+                  className={cn("w-full border-l border-b", {
+                    "border-l-0": columnIndex % columnLength === 0,
+                    "border-b-0": rowIndex + 1 === rowLength,
+                  })}
+                >
+                  <CalendarCell cell={date} />
                 </div>
               );
             })}
           </React.Fragment>
         );
       })}
+    </div>
+  );
+}
+
+function CalendarCell({ cell }: { cell: CalendarCell }) {
+  return (
+    <div>
+      <span>{cell.meta.date}일</span>
+      <ul>
+        {cell.todos.map((todo) => {
+          return <li key={todo.id}>{todo.flakeName}</li>;
+        })}
+      </ul>
     </div>
   );
 }
