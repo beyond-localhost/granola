8eaf7b9584581f8a08ffd07113ca36ad7e5d15af
steven semyung oh <semyung.dev@gmail.com>
2025-01-11 20:01:26 +0900

    append globalout to the main layout

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -13,23 +13,28 @@ import {
   FlakeContextProvider,
   TodoContextProvider,
 } from "#/lib/state";
+
 import { SidebarProvider } from "#/components/ui/sidebar";
+import { GlobalOutletProvider, GlobalOutlet } from "#/components/portal";
 
 export const Route = createRootRoute({
   component: () => (
-    <BowlContextProvider initialData={initialBowlsPromise}>
-      <FlakeContextProvider initialFlakes={initialFlakesPromise}>
-        <TodoContextProvider initialData={initialTodosPromise}>
-          <SidebarProvider>
-            <AppSidebar />
-            <main className="w-full">
-              <Outlet />
-            </main>
-          </SidebarProvider>
+    <GlobalOutletProvider>
+      <BowlContextProvider initialData={initialBowlsPromise}>
+        <FlakeContextProvider initialFlakes={initialFlakesPromise}>
+          <TodoContextProvider initialData={initialTodosPromise}>
+            <SidebarProvider>
+              <AppSidebar />
+              <main className="w-full">
+                <Outlet />
+              </main>
+            </SidebarProvider>
 
-          <TanStackRouterDevtools />
-        </TodoContextProvider>
-      </FlakeContextProvider>
-    </BowlContextProvider>
+            <TanStackRouterDevtools />
+          </TodoContextProvider>
+        </FlakeContextProvider>
+      </BowlContextProvider>
+      <GlobalOutlet />
+    </GlobalOutletProvider>
   ),
 });
