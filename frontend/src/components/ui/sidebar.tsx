c7d0576d61653a0587d9265958e4a3c6c0f1d309
steven semyung oh <semyung.dev@gmail.com>
2025-01-12 13:03:18 +0900

    add scroll lock

--- frontend/src/components/ui/sidebar.tsx
+++ frontend/src/components/ui/sidebar.tsx
@@ -179,7 +179,7 @@ const Sidebar = React.forwardRef<
       return (
         <div
           className={cn(
-            "flex h-full w-[--sidebar-width] flex-col bg-sidebar text-sidebar-foreground",
+            "flex h-full w-[--sidebar-width] flex-col bg-sidebar text-sidebar-foreground isolate",
             className
           )}
           ref={ref}
