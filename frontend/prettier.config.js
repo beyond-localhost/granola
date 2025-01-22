21e69d05d7a0df1f7399d1a20847315c4a5b63b3
steven semyung oh <semyung.dev@gmail.com>
2025-01-22 10:14:18 +0900

    (frontend) setup prettier

---
+++ frontend/prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: false,
+  printWidth: 90,
+  endOfLine: "lf",
+  trailingComma: "es5",
+};
+
+export default config;
