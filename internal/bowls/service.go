adfb54859b928c97f703d67ce85b491e4a41be18
steven semyung oh <semyung.dev@gmail.com>
2025-01-23 10:47:02 +0900

    migrate to sqlc without side-effect

--- internal/bowls/service.go
+++ internal/bowls/service.go
@@ -1,29 +1,40 @@
 package bowls
 
+import (
+	"database/sql"
+)
+
 type BowlsService struct {
-	repo BowlRepository
+	a Adapter
 }
 
-func NewBowlsService(repo BowlRepository) *BowlsService {
-	return &BowlsService{repo}
+func NewBowlsService(a Adapter) *BowlsService {
+	return &BowlsService{a}
 }
 
 func (s *BowlsService) GetAll() ([]Bowl, error) {
-	return s.repo.GetAll()
+	return s.a.q.GetAll(s.a.ctx)
 }
 
-func (s *BowlsService) GetById(id int64) (*Bowl, error) {
-	return s.repo.GetById(id)
+func (s *BowlsService) GetById(id int64) (Bowl, error) {
+	return s.a.q.GetById(s.a.ctx, id)
 }
 
-func (s *BowlsService) Create(name string, description *string) (*Bowl, error) {
-	return s.repo.Create(name, description)
-}
+func (s *BowlsService) Create(name string, description *string) (Bowl, error) {
+	descriptionVal := *description
+	descriptionEmtpy := description == nil
 
-func (s *BowlsService) UpdateById(id int64, update BowlUpdate) (*Bowl, error) {
-	return s.repo.UpdateById(id, update)
+	params := CreateParams{
+		Name: name,
+		Description: sql.NullString{
+			String: descriptionVal,
+			Valid: !descriptionEmtpy,
+		},
+	}
+
+	return s.a.q.Create(s.a.ctx, params)
 }
 
 func (s *BowlsService) DeleteById(id int64) error {
-	return s.repo.DeleteById(id)
+	return s.a.q.DeleteById(s.a.ctx, id)
 }
